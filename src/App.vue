0b2d7a04edfdc49944605d6a76c0727c18e95c6d
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-07-28 16:14:21 +0330

    #1 displaying a list of pdf books

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import {  RouterView } from 'vue-router'
-import AppHeader from './components/AppHeader.vue';
 </script>
 
 <template>
   <header>
     <img alt="نسک‌بان" class="logo" src="@/assets/logo.svg" width="125" height="125" />
     <div class="wrapper">
-      <AppHeader msg="نسک‌بان" />
+      <h1 class="green">نسک‌بان</h1>
     </div>
   </header>
 
@@ -17,64 +16,16 @@ import AppHeader from './components/AppHeader.vue';
 <style scoped>
 
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  margin: auto;
+  text-align: center;
+}
+ .wrapper{
+  margin: auto;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: auto;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,48 @@
+<!--
+This example fetches latest Vue.js commits data from GitHub’s API and displays them as a list.
+You can switch between the two branches.
+-->
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, watchEffect } from 'vue'
+
+const API_URL = `https://api.naskban.ir/api/pdf`
+
+const pdfs = ref(null)
+
+watchEffect(async () => {
+  // this effect will run immediately and then
+  // re-run whenever currentBranch.value changes
+  const url = `${API_URL}?PageNumber=20&PageSize=20`
+  pdfs.value = await (await fetch(url)).json()
+})
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h3>تازه‌ترین کتاب‌های افزوده شده</h3>
+  <div class="flex-container">
+    <div class="pdf" v-for="pdf in pdfs" :key="pdf.id">
+      <a :href="pdf.id"><img :src="pdf.extenalCoverImageUrl" :alt="pdf.title" /> </a>
+      <a :href="pdf.id">{{ pdf.title }}</a>
+    </div>
+  </div>
 </template>
+
+<style>
+a {
+  text-decoration: none;
+  color: #42b883;
+}
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+h3{
+  text-align: center;
+}
+.pdf{
+  text-align: center;
+  max-width: 300px;
+}
+</style>
