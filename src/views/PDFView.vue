834506f64ca12ab066d434869604527ea8ebb72f
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-07-31 11:28:38 +0330

    #2 page number in url is working

--- src/views/PDFView.vue
+++ src/views/PDFView.vue
@@ -8,7 +8,8 @@ const route = useRoute()
 const loading = ref(false)
 const pdf = ref(null)
 const pdfFile = ref(null)
-const pageNumber = ref(1)
+const pageNumber = ref(null)
+const pageCount = ref(null)
 
 function en2fa(num) {
   let arr = []
@@ -27,43 +28,67 @@ watchEffect(async () => {
   pdfFile.value = usePDF({
     url: pdf.value.externalPDFFileUrl
   })
-  if (route.params.page != null) {
-    pageNumber.value =  parseInt(route.params.page)
+  if (pageNumber.value == null) {
+    if (route.params.page != null) {
+      pageNumber.value = parseInt(route.params.page)
+    } else {
+      pageNumber.value = 1
+    }
   }
+  pageCount.value = pdfFile.value.pages
 
   if (pageNumber.value == 1) {
     document.title = 'نسک‌بان - ' + pdf.value.title
+    window.history.pushState({}, '', '/' + pdf.value.id.toString() + '/1')
   } else {
     document.title =
       'نسک‌بان - ' + pdf.value.title + ' - صفحهٔ ' + en2fa(pageNumber.value.toString())
-  }
-})
-
-function onLoaded(){
-  loading.value = false
-}
-
-function setPage(newPageNumber) {
-  if (newPageNumber < 1) return
-  if (newPageNumber > pdfFile.value.pages) return
-  pageNumber.value = newPageNumber
-  if (pageNumber.value == 1) {
-    window.history.pushState({}, '', '/' + pdf.value.id.toString() + '/1')
-    document.title = 'نسک‌بان - ' + pdf.value.title
-  } else {
     window.history.pushState(
       {},
       '',
       '/' + pdf.value.id.toString() + '/' + pageNumber.value.toString()
     )
-    'نسک‌بان - ' + pdf.value.title + ' - صفحهٔ ' + en2fa(pageNumber.value.toString())
+  }
+})
+
+function onLoaded() {
+  loading.value = false
+}
+function updatePageNumber(value){
+  if (value == 1) {
+    document.title = 'نسک‌بان - ' + pdf.value.title
+    window.history.pushState({}, '', '/' + pdf.value.id.toString() + '/1')
+  } else {
+    document.title =
+      'نسک‌بان - ' + pdf.value.title + ' - صفحهٔ ' + en2fa(value.toString())
+    window.history.pushState(
+      {},
+      '',
+      '/' + pdf.value.id.toString() + '/' + value.toString()
+    )
   }
 }
 </script>
 
 <template>
-  <button @click="setPage(parseInt(pageNumber) + 1)">+ Page</button>
-  <button @click="setPage(parseInt(pageNumber) - 1)">- Page</button>
-  <q-spinner-hourglass v-if="loading" color="green" size="4em" />
-  <VuePDF v-if="pdfFile != null" :pdf="pdfFile.pdf" :page="pageNumber" @loaded="onLoaded" />
+  <div class="q-pa-lg flex flex-center">
+    <q-spinner-hourglass v-if="loading" color="green" size="4em" />
+    <div class="q-pa-lg flex flex-center justify-center centers">
+      <q-pagination
+        v-if="pdfFile != null"
+        v-model="pageNumber"
+        :max="pdfFile.pages"
+        input
+        input-class="text-orange-10"
+        icon-last="skip_previous"
+        icon-first="skip_next"
+        icon-next="fast_rewind"
+        icon-prev="fast_forward"
+        @update:model-value="updatePageNumber"
+      />
+    </div>
+    <div class="full-width q-pa-lg flex flex-center justify-center centers">
+      <VuePDF v-if="pdfFile != null" :pdf="pdfFile.pdf" :page="pageNumber" @loaded="onLoaded" />
+    </div>
+  </div>
 </template>
