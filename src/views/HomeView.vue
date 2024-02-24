bb71233207118749bbc98d4280634a7a46061407
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-02-24 18:55:07 +0330

    1000  per page -> 100

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,7 @@ const pageNumber = ref(null)
 const pdfs = ref(null)
 const pageCount = ref(1)
 const searchTerm = ref('')
-const pageSize = 1000
+const pageSize = 100
 const userInfo = ref(null)
 
 bus.on('user-logged-out', () => {
